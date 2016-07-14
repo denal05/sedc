2a383aa58840bd3998fe0d897aa9bb4f4ae11a47
Denis Aleksandrov <denal05@gmail.com>
2016-07-14 19:08:03 +0200

    Added three kinds of timers in CategoriesInObjectiveC example.

--- ios-advanced/CategoriesInObjectiveC/CategoriesInObjectiveC/ViewController.m
+++ ios-advanced/CategoriesInObjectiveC/CategoriesInObjectiveC/ViewController.m
@@ -20,6 +20,33 @@
     [_myLabel setDefaultSettings];
     [_myTextView setRandomColor];
     [_myTextView removeSpaces];
+    
+    // Timer #1
+    [self performSelector:@selector(f1) withObject:nil afterDelay:3];
+    
+    // To cancel performSelector:afterDelay:
+    ////[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(f1) object:nil];
+    
+    // Timer #2
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(f2) userInfo:nil repeats:NO];
+    
+    // To cancel NSTimer
+    ////[timer invalidate];
+    
+    // Timer #3
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(7 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        printf(">>>timer3\n");
+    });
+}
+
+-(void)f1
+{
+    printf(">>>timer1\n");
+}
+
+-(void)f2
+{
+    printf(">>>timer2\n");
 }
 
 - (void)didReceiveMemoryWarning {
